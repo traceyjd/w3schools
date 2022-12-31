25ff149e4e48293b24773e8114f6ad40c0df7b6c
Tracey <tracey.tdoherty@gmail.com>
2022-12-31 15:47:26 +0000

    Variables and Data Types

--- Var.py
+++ Var.py
@@ -61,3 +61,80 @@ x = "Python is awesome"
 print(x)
 
 # In the print() function, you output multiple variables, separated by a comma:
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+# You can also use the + operator to output multiple variables:
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+# Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome".
+
+# For numbers, the + character works as a mathematical operator:
+x = 5
+y = 10
+print(x + y)
+
+#In the print() function, when you try to combine a string and a number with the + operator, Python will give you an error:
+x = 5
+y = "John"
+print(x + y)
+
+#The best way to output multiple variables in the print() function is to separate them with commas, which even support different data types:
+x = 5
+y = "John"
+print(x, y)
+
+# Global Variables
+# Variables that are created outside of a function (as in all of the examples above) are known as global variables.
+#
+# Global variables can be used by everyone, both inside of functions and outside.
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+# If you create a variable with the same name inside a function, this variable will be local, and can only be used inside the function.
+# The global variable with the same name will remain as it was, global and with the original value.
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+# Normally, when you create a variable inside a function, that variable is local, and can only be used inside that function.
+# To create a global variable inside a function, you can use the global keyword.
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+# Also, use the global keyword if you want to change a global variable inside a function.
+# Example
+# To change the value of a global variable inside a function, refer to the variable by using the global keyword:
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+# Exercises
+carname = "Volvo"
+print(carname)
--- intro.py
+++ intro.py
@@ -1,1 +1,10 @@
 print('Hello World!')
+
+# Since Python will ignore string literals that are not assigned to a variable, you can add a multiline string (triple quotes) in your code,
+# and place your comment inside it:
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
