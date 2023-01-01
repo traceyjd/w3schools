e915deeeb1a84113fec4cc69219d8b8f531781d3
Tracey <tracey.tdoherty@gmail.com>
2023-01-01 16:40:20 +0000

    Variables and Data Types

--- Booleans.py
+++ Booleans.py
@@ -22,7 +22,7 @@ b = 33
 # Example
 #Evaluate a string and a number:
 print(bool("B"))
-print(bool(15))
+print(bool(0))
 
 # Example
 # Evaluate two variables:
@@ -31,3 +31,20 @@ y = 15
 print(bool(x))
 print(bool(y))
 
+# Most Values are True
+# Almost any value is evaluated to True if it has some sort of content.
+# Any string is True, except empty strings.
+# Any number is True, except 0.
+# Any list, tuple, set, and dictionary are True, except empty ones.
+# Example
+# The following will return True:
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+# Some Values are False
+# In fact, there are not many values that evaluate to False, except empty
+# values, such as (), [], {}, "", the number 0, and the value None. And of course the value False evaluates to False.
+bool(False)
+bool(None)
+bool(0)
