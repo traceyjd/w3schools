8ea4a12b712bb8b4272f1d589b8ada05dfb07e0c
Tracey <tracey.tdoherty@gmail.com>
2023-01-01 19:38:06 +0000

    Variables and Data Types

--- Lists.py
+++ Lists.py
@@ -115,4 +115,32 @@ print(thislist[-2])
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[2:5])
 
+# Check if Item Exists
+# To determine if a specified item is present in a list use the in keyword:
+#
+# Example
+# Check if "apple" is present in the list:
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+
+# Change Item Value
+# To change the value of a specific item, refer to the index number:
+#
+# Example
+# Change the second item:
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+# Change a Range of Item Values
+# To change the value of items within a specific range, define a list with the new values, and refer to the range of index numbers where you want to insert the new values:
+#
+# Example
+# Change the values "banana" and "cherry" with the values "blackcurrant" and "watermelon":
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+
 
