b4a9ac4e0bf9149bd4902153d215f9fbdb21bec7
Tracey <tracey.tdoherty@gmail.com>
2023-01-01 21:17:07 +0000

    Variables and Data Types

--- Lists.py
+++ Lists.py
@@ -341,7 +341,6 @@ print(newlist)
 #
 # Example
 # Sort the list alphabetically:
-
 thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
 thislist.sort()
 print(thislist)
@@ -365,4 +364,27 @@ print(thislist)
 # Sort the list descending:
 thislist = [100, 50, 65, 82, 23]
 thislist.sort(reverse = True)
+print(thislist)
+
+# Customize Sort Function
+# You can also customize your own function by using the keyword argument key = function.
+#
+# The function will return a number that will be used to sort the list (the lowest number first):
+#
+# Example
+# Sort the list based on how close the number is to 50:
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
+# Case Insensitive Sort
+# By default the sort() method is case sensitive, resulting in all capital letters being sorted before lower case letters:
+#
+# Example
+# Case sensitive sorting can give an unexpected result:
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
 print(thislist)
