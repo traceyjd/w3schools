5683d4db59bb737b82f7dbcfe1471b100850d356
Tracey <tracey.tdoherty@gmail.com>
2023-01-01 21:35:23 +0000

    Variables and Data Types

--- Lists.py
+++ Lists.py
@@ -388,3 +388,95 @@ print(thislist)
 thislist = ["banana", "Orange", "Kiwi", "cherry"]
 thislist.sort()
 print(thislist)
+
+# Luckily we can use built-in functions as key functions when sorting a list.
+#
+# So if you want a case-insensitive sort function, use str.lower as a key function:
+#
+# Example
+# Perform a case-insensitive sort of the list:
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+
+# Reverse Order
+# What if you want to reverse the order of a list, regardless of the alphabet?
+#
+# The reverse() method reverses the current sorting order of the elements.
+#
+# Example
+# Reverse the order of the list items:
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+
+# Copy a List
+# You cannot copy a list simply by typing list2 = list1, because: list2 will only be a reference to list1, and changes made in list1 will automatically also be made in list2.
+#
+# There are ways to make a copy, one way is to use the built-in List method copy().
+#
+# Example
+# Make a copy of a list with the copy() method:
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+# Another way to make a copy is to use the built-in method list().
+#
+# Example
+# Make a copy of a list with the list() method:
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+# Join Two Lists
+# There are several ways to join, or concatenate, two or more lists in Python.
+#
+# One of the easiest ways are by using the + operator.
+#
+# Example
+# Join two list:
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+# Another way to join two lists is by appending all the items from list2 into list1, one by one:
+#
+# Example
+# Append list2 into list1:
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+
+# Or you can use the extend() method, which purpose is to add elements from one list to another list:
+#
+# Example
+# Use the extend() method to add list2 at the end of list1:
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
+
+# List Methods
+# Python has a set of built-in methods that you can use on lists.
+#
+# Method	Description
+# append()	Adds an element at the end of the list
+# clear()	Removes all the elements from the list
+# copy()	Returns a copy of the list
+# count()	Returns the number of elements with the specified value
+# extend()	Add the elements of a list (or any iterable), to the end of the current list
+# index()	Returns the index of the first element with the specified value
+# insert()	Adds an element at the specified position
+# pop()	Removes the element at the specified position
+# remove()	Removes the item with the specified value
+# reverse()	Reverses the order of the list
+# sort()	Sorts the list
+
