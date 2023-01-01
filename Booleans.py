c0788b2c703bf60204238c273e39beaf2a769aea
Tracey <tracey.tdoherty@gmail.com>
2023-01-01 16:50:15 +0000

    Variables and Data Types

--- Booleans.py
+++ Booleans.py
@@ -48,3 +48,52 @@ bool(["apple", "cherry", "banana"])
 bool(False)
 bool(None)
 bool(0)
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+# One more value, or object in this case, evaluates to False, and that is if you have an object that is made from
+# a class with a __len__ function that returns 0 or False:
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+# Functions can Return a Boolean
+# You can create functions that returns a Boolean Value:
+def myFunction() :
+  return True
+
+print(myFunction())
+
+# You can execute code based on the Boolean answer of a function:
+# Example
+# Print "YES!" if the function returns True, otherwise print "NO!":
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+def myFunction() :
+  return False
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+# Python also has many built-in functions that return a boolean value, like the isinstance() function, which can be used to determine if an object is of a certain data type:
+# Example
+# Check if an object is an integer or not:
+x = 200
+print(isinstance(x, int))
