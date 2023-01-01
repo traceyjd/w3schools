081c431baed86e4e8cca3fc6e134285b542b9185
Tracey <tracey.tdoherty@gmail.com>
2023-01-01 16:34:23 +0000

    Variables and Data Types

--- Booleans.py
+++ Booleans.py
@@ -17,3 +17,17 @@ b = 33
  else:
    print("b is not greater than a")
 
+# Evaluate Values and Variables
+# The bool() function allows you to evaluate any value, and give you True or False in return,
+# Example
+#Evaluate a string and a number:
+print(bool("B"))
+print(bool(15))
+
+# Example
+# Evaluate two variables:
+x = "Hello"
+y = 15
+print(bool(x))
+print(bool(y))
+
