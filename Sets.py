251b21fa572c06c8025f744b320688b5dd826d85
Tracey <tracey.tdoherty@gmail.com>
2023-01-02 15:32:16 +0000

    Variables and Data Types

--- Sets.py
+++ Sets.py
@@ -146,3 +146,143 @@ thisset = {"apple", "banana", "cherry"}
 mylist = ["kiwi", "orange"]
 thisset.update(mylist)
 print(thisset)
+
+#
+# Remove Item
+# To remove an item in a set, use the remove(), or the discard() method.
+#
+# Example
+# Remove "banana" by using the remove() method:
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset)
+
+#  If the item to remove does not exist, remove() will raise an error.
+
+# Example
+# Remove "banana" by using the discard() method:
+thisset = {"apple", "banana", "cherry"}
+thisset.discard("banana")
+print(thisset)
+
+# Note: If the item to remove does not exist, discard() will NOT raise an error.
+#
+# You can also use the pop() method to remove an item, but this method will remove the last item. Remember that sets are unordered, so you will not know what item that gets removed.
+#
+# The return value of the pop() method is the removed item.
+#
+# Example
+# Remove the last item by using the pop() method:
+thisset = {"apple", "banana", "cherry"}
+x = thisset.pop()
+print(x)
+print(thisset)
+
+# Note: Sets are unordered, so when using the pop() method, you do not know which item that gets removed.
+#
+# Example
+# The clear() method empties the set:
+thisset = {"apple", "banana", "cherry"}
+thisset.clear()
+print(thisset)
+
+
+# Example
+# The del keyword will delete the set completely:
+thisset = {"apple", "banana", "cherry"}
+del thisset
+print(thisset)
+
+
+# Loop Items
+# You can loop through the set items by using a for loop:
+#
+# Example
+# Loop through the set, and print the values:
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+  print(x)
+
+
+# Join Two Sets
+# There are several ways to join two or more sets in Python.
+#
+# You can use the union() method that returns a new set containing all items from both sets, or the update()
+# method that inserts all the items from one set into another:
+#
+# Example
+# The union() method returns a new set with all items from both sets:
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set3 = set1.union(set2)
+print(set3)
+
+
+# Example
+# The update() method inserts the items in set2 into set1:
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set1.update(set2)
+print(set1)
+
+# Both union() and update() will exclude any duplicate items.
+
+
+
+# Keep ONLY the Duplicates
+# The intersection_update() method will keep only the items that are present in both sets.
+#
+# Example
+# Keep the items that exist in both set x, and set y:
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.intersection_update(y)
+print(x)
+
+
+
+# The intersection() method will return a new set, that only contains the items that are present in both sets.
+#
+# Example
+# Return a set that contains the items that exist in both set x, and set y:
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.intersection(y)
+print(z)
+
+
+# Keep All, But NOT the Duplicates
+# The symmetric_difference_update() method will keep only the elements that are NOT present in both sets.
+#
+# Example
+# Keep the items that are not present in both sets:
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.symmetric_difference_update(y)
+print(x)
+
+# The symmetric_difference() method will return a new set, that contains only the elements that are NOT
+# present in both sets.
+
+
+# Set Methods
+# Python has a set of built-in methods that you can use on sets.
+#
+# Method	Description
+# add()	Adds an element to the set
+# clear()	Removes all the elements from the set
+# copy()	Returns a copy of the set
+# difference()	Returns a set containing the difference between two or more sets
+# difference_update()	Removes the items in this set that are also included in another, specified set
+# discard()	Remove the specified item
+# intersection()	Returns a set, that is the intersection of two other sets
+# intersection_update()	Removes the items in this set that are not present in other, specified set(s)
+# isdisjoint()	Returns whether two sets have a intersection or not
+# issubset()	Returns whether another set contains this set or not
+# issuperset()	Returns whether this set contains another set or not
+# pop()	Removes an element from the set
+# remove()	Removes the specified element
+# symmetric_difference()	Returns a set with the symmetric differences of two sets
+# symmetric_difference_update()	inserts the symmetric differences from this set and another
+# union()	Return a set containing the union of sets
+# update()	Update the set with the union of this set and others
