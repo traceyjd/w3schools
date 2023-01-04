6df5c0e68de83449ba0dc55ad9f8e1e2ff14dd46
Tracey <tracey.tdoherty@gmail.com>
2023-01-04 20:10:07 +0000

    Variables and Data Types

--- Dictionaries.py
+++ Dictionaries.py
@@ -317,3 +317,48 @@ thisdict = {
 thisdict.update({"year": 2020})
 
 
+# Adding Items
+# Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+#
+# Example
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+
+# Update Dictionary
+# The update() method will update the dictionary with the items from a given argument. If the item does not exist, the item will be added.
+#
+# The argument must be a dictionary, or an iterable object with key:value pairs.
+
+# Example
+# Add a color item to the dictionary by using the update() method:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"})
+
+
+# Removing Items
+# There are several methods to remove items from a dictionary:
+#
+# Example
+# The pop() method removes the item with the specified key name:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+
+
+# Example
+# The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead):
