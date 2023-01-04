e6efe76a121fff252e717b786cfeee25e5498c9e
Tracey <tracey.tdoherty@gmail.com>
2023-01-04 20:12:26 +0000

    Variables and Data Types

--- Dictionaries.py
+++ Dictionaries.py
@@ -362,3 +362,53 @@ print(thisdict)
 
 # Example
 # The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead):
+
+Example
+The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead):
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+
+
+
+# Example
+# The del keyword removes the item with the specified key name:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+
+
+Example
+The del keyword can also delete the dictionary completely:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict
+print(thisdict) #this will cause an error because "thisdict" no longer exists.
+
+
+
+# Example
+# The clear() method empties the dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
+
+
