bce78db2826bf42c173dc3e28e2a48ae94f0c5b5
Tracey <tracey.tdoherty@gmail.com>
2023-01-04 19:08:08 +0000

    Variables and Data Types

--- Dictionaries.py
+++ Dictionaries.py
@@ -107,3 +107,32 @@ print(thisdict)
 #
 # When choosing a collection type, it is useful to understand the properties of that type. Choosing the right type for
 # a particular data set could mean retention of meaning, and, it could mean an increase in efficiency or security.
+
+
+
+# Accessing Items
+# You can access the items of a dictionary by referring to its key name, inside square brackets:
+#
+# Example
+# Get the value of the "model" key:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+print(x)
+
+
+
+# There is also a method called get() that will give you the same result:
+#
+# Example
+# Get the value of the "model" key:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict.get("model")
+print(x)
