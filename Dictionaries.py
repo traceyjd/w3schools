90969c29d25da6183ad6bfa8aa12c531ed26dda8
Tracey <tracey.tdoherty@gmail.com>
2023-01-05 15:23:46 +0000

    Variables and Data Types

--- Dictionaries.py
+++ Dictionaries.py
@@ -465,8 +465,105 @@ for x in thisdict.keys():
 
 
 
-Example
-Loop through both keys and values, by using the items() method:
-
+# Example
+# Loop through both keys and values, by using the items() method:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
 for x, y in thisdict.items():
   print(x, y)
+
+
+
+# Copy a Dictionary
+# You cannot copy a dictionary simply by typing dict2 = dict1, because: dict2 will only be a reference to dict1,
+# and changes made in dict1 will automatically also be made in dict2.
+#
+# There are ways to make a copy, one way is to use the built-in Dictionary method copy().
+#
+# Example
+# Make a copy of a dictionary with the copy() method:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+
+# Example
+# Make a copy of a dictionary with the dict() function:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+
+
+
+# Nested Dictionaries
+# A dictionary can contain dictionaries, this is called nested dictionaries.
+#
+# Example
+# Create a dictionary that contain three dictionaries:
+
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+  print(myfamily)
+
+
+# Or, if you want to add three dictionaries into a new dictionary:
+#
+# Example
+# Create three dictionaries, then create one dictionary that will contain the other three dictionaries:
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+print(myfamily)
+
+# Dictionary Methods
+# Python has a set of built-in methods that you can use on dictionaries.
+#
+# Method	Description
+# clear()	Removes all the elements from the dictionary
+# copy()	Returns a copy of the dictionary
+# fromkeys()	Returns a dictionary with the specified keys and value
+# get()	Returns the value of the specified key
+# items()	Returns a list containing a tuple for each key value pair
+# keys()	Returns a list containing the dictionary's keys
+# pop()	Removes the element with the specified key
+# popitem()	Removes the last inserted key-value pair
+# setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+# update()	Updates the dictionary with the specified key-value pairs
+# values()	Returns a list of all the values in the dictionary
