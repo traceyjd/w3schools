2b35a55f3ca6224c118cb34cdf4776daa71aaaf6
Tracey <tracey.tdoherty@gmail.com>
2023-01-05 16:14:43 +0000

    Variables and Data Types

--- ifelse.py
+++ ifelse.py
@@ -130,18 +130,20 @@ c = 500
 if a > b or a > c:
   print("At least one of the conditions is True")
 
+
+
 # Nested If
 # You can have if statements inside if statements, this is called nested if statements.
 #
 # Example
 x = 41
-
 if x > 10:
   print("Above ten,")
   if x > 20:
     print("and also above 20!")
   else:
     print("but not above 20.")
+
 
 
 # The pass Statement
