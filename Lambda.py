83f77d5d03080b35f54ef7ee120e1d944bfc8d99
Tracey <tracey.tdoherty@gmail.com>
2023-01-07 14:48:30 +0000

    Variables and Data Types

--- Lambda.py
+++ Lambda.py
@@ -55,3 +55,17 @@ mytripler = myfunc(3)
 print(mytripler(11))
 
 # Or, use the same function definition to make both functions, in the same program:
+
+# Or, use the same function definition to make both functions, in the same program:
+#
+# Example
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
+
+# Use lambda functions when an anonymous function is required for a short period of time.
