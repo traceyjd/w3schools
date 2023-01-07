15f77e8e8c523da29e2f6e5cff99edd865d89b95
Tracey <tracey.tdoherty@gmail.com>
2023-01-07 14:01:41 +0000

    Variables and Data Types

--- Funtions.py
+++ Funtions.py
@@ -87,3 +87,48 @@ def my_function(*kids):
   print("The youngest child is " + kids[2])
 
 my_function("Emil", "Tobias", "Linus")
+
+
+# Keyword Arguments
+# You can also send arguments with the key = value syntax.
+#
+# This way the order of the arguments does not matter.
+#
+# Example
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+# The phrase Keyword Arguments are often shortened to kwargs in Python documentations.
+
+
+# Arbitrary Keyword Arguments, **kwargs
+# If you do not know how many keyword arguments that will be passed into your function, add two asterisk: ** before the parameter name in the function definition.
+#
+# This way the function will receive a dictionary of arguments, and can access the items accordingly:
+#
+# Example
+# If the number of keyword arguments is unknown, add a double ** before the parameter name:
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Jones")
+
+# Arbitrary Kword Arguments are often shortened to **kwargs in Python documentations.
+
+
+# Default Parameter Value
+# The following example shows how to use a default parameter value.
+#
+# If we call the function without argument, it uses the default value:
+#
+# Example
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
