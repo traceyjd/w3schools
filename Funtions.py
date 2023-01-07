e315ffa16cad47cd87f38912aee3216817dbcf41
Tracey <tracey.tdoherty@gmail.com>
2023-01-07 14:12:47 +0000

    Variables and Data Types

--- Funtions.py
+++ Funtions.py
@@ -132,3 +132,38 @@ my_function("Sweden")
 my_function("India")
 my_function()
 my_function("Brazil")
+
+
+# Passing a List as an Argument
+# You can send any data types of argument to a function (string, number, list, dictionary etc.), and it will be treated as the same data type inside the function.
+#
+# E.g. if you send a List as an argument, it will still be a List when it reaches the function:
+#
+# Example
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+
+
+# Return Values
+# To let a function return a value, use the return statement:
+#
+# Example
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+
+# The pass Statement
+# function definitions cannot be empty, but if you for some reason have a function definition with no content, put in the pass statement to avoid getting an error.
+#
+# Example
+def myfunction():
+  pass
