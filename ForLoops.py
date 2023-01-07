33399d6aacb53dea4930f0f3f9e2f6bb53676c34
Tracey <tracey.tdoherty@gmail.com>
2023-01-07 13:44:18 +0000

    Variables and Data Types

--- ForLoops.py
+++ ForLoops.py
@@ -84,6 +84,7 @@ for x in range(2, 30, 3):
   print(x)
 
 
+
 # Else in For Loop
 # The else keyword in a for loop specifies a block of code to be executed when the loop is finished:
 #
@@ -95,6 +96,7 @@ else:
   print("Finally finished!")
 
 
+
 # Note: The else block will NOT be executed if the loop is stopped by a break statement.
 #
 # Example
@@ -104,6 +106,7 @@ for x in range(6):
   print(x)
 else:
   print("Finally finished!")
+
 
 
 # Nested Loops
