150be0025725a7c5d2c7514b7a36df59cfd0a5ce
Tracey <tracey.tdoherty@gmail.com>
2023-01-10 15:52:11 +0000

    Variables and Data Types

---
+++ Math.py
@@ -0,0 +1,46 @@
+
+
+# The Math Module
+# Python has also a built-in module called math, which extends the list of mathematical functions.
+#
+# To use it, you must import the math module:
+#
+# import math
+# When you have imported the math module, you can start using methods and constants of the module.
+#
+# The math.sqrt() method for example, returns the square root of a number:
+#
+# Example
+import math
+
+x = math.sqrt(64)
+
+print(x)
+
+
+# The math.ceil() method rounds a number upwards to its nearest integer, and the math.floor() method rounds a number
+# downwards to its nearest integer, and returns the result:
+#
+# Example
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+
+# The math.pi constant, returns the value of PI (3.14...):
+#
+# Example
+import math
+
+x = math.pi
+
+print(x)
+
+
+# Complete Math Module Reference
+# In our Math Module Reference you will find a complete reference of all methods and constants that belongs to the Math
+# module.
