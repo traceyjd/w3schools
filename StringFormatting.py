ce75340f8fa7ca70767650aca8af770a8150f85a
Tracey <tracey.tdoherty@gmail.com>
2023-01-12 12:01:08 +0000

    Variables and Data Types

--- StringFormatting.py
+++ StringFormatting.py
@@ -128,9 +128,72 @@ import re
 txt = "The rain in Spain"
 x = re.sub("\s", "9", txt)
 print(x)
-You can control the number of replacements by specifying the count parameter:
 
-Example
-Replace the first 2 occurrences:
+# You can control the number of replacements by specifying the count parameter:
+#
+# Example
+# Replace the first 2 occurrences:
+import re
+
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt, 2)
+print(x)
+
+# Match Object
+# A Match Object is an object containing information about the search and the result.
+#
+# Note: If there is no match, the value None will be returned, instead of the Match Object.
+#
+# Example
+# Do a search that will return a Match Object:
+import re
+
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x) #this will print an object
+
+# Note: If there is no match, the value None will be returned, instead of the Match Object.
+
+# Example
+# Do a search that will return a Match Object:
+import re
+
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x) #this will print an object
+
+# The Match object has properties and methods used to retrieve information about the search, and the result:
+#
+# .span() returns a tuple containing the start-, and end positions of the match.
+# .string returns the string passed into the function
+# .group() returns the part of the string where there was a match
 
 
+# Example
+# Print the position (start- and end-position) of the first match occurrence.
+#
+# The regular expression looks for any words that starts with an upper case "S":
+import re
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.span())
+
+# Example
+# Print the string passed into the function:
+import re
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.string)
+
+# Example
+# Print the part of the string where there was a match.
+#
+# The regular expression looks for any words that starts with an upper case "S":
+import re
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.group())
+Note: If there is no match, the value None will be returned, instead of the Match Object.
