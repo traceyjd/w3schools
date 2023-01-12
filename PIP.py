d3c665f9b3a21087be4429803716c79ad2eea1af
Tracey <tracey.tdoherty@gmail.com>
2023-01-12 13:36:44 +0000

    Variables and Data Types

---
+++ PIP.py
@@ -0,0 +1,64 @@
+
+
+# Python PIP
+# What is PIP?
+# PIP is a package manager for Python packages, or modules if you like.
+#
+# Note: If you have Python version 3.4 or later, PIP is included by default.
+#
+# What is a Package?
+# A package contains all the files you need for a module.
+#
+# Modules are Python code libraries you can include in your project.
+#
+# Check if PIP is Installed
+
+
+# Check if PIP is Installed
+# Navigate your command line to the location of Python's script directory, and type the following:
+#
+# Example
+# Check PIP version:
+
+#  C:\Users\Your Name\AppData\Local\Programs\Python\Python36-32\Scripts>pip --version
+
+
+# Install PIP
+# If you do not have PIP installed, you can download and install it from this page: https://pypi.org/project/pip/
+#
+# Download a Package
+# Downloading a package is very easy.
+#
+# Open the command line interface and tell PIP to download the package you want.
+
+# Using a Package
+# Once the package is installed, it is ready to use.
+#
+# Import the "camelcase" package into your project.
+#
+# Example
+# Import and use "camelcase":
+import camelcase
+
+c = camelcase.CamelCase()
+
+txt = "hello world"
+
+print(c.hump(txt))
+
+# List Packages
+# Use the list command to list all the packages installed on your system:
+#
+# Example
+# List installed packages:
+#
+# C:\Users\Your Name\AppData\Local\Programs\Python\Python36-32\Scripts>pip list
+# Result:
+
+Package         Version
+-----------------------
+camelcase       0.2
+mysql-connector 2.1.6
+pip             18.1
+pymongo         3.6.1
+setuptools      39.0.1
