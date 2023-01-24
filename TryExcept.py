9a15ec7abb96700d6577018c023266d6cc1b6aab
Tracey <tracey.tdoherty@gmail.com>
2023-01-24 16:03:36 +0000

    Variables and Data Types

--- TryExcept.py
+++ TryExcept.py
@@ -100,6 +100,7 @@ if x < 0:
   raise Exception("Sorry, no numbers below zero")
 
 
+
 # The raise keyword is used to raise an exception.
 #
 # You can define what kind of error to raise, and the text to print to the user.
